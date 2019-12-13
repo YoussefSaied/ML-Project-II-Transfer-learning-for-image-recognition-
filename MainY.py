1d2a898445790773538566d33eeed03e7cb6ca09
Youssef <youssef.saied@epfl.ch>
2019-12-13 19:45:15 +0100

    fix

--- MainY.py
+++ MainY.py
@@ -59,8 +59,8 @@ test_batch_size = 1
 # samplerv= BalancedBatchSampler2(trainset)
 # samplertest = BalancedBatchSampler2(testset)
 
-trainloader = torch.utils.data.DataLoader(trainset,shuffle=true, batch_size= batch_sizev, num_workers=2)
-testloader = torch.utils.data.DataLoader(testset ,shuffle =true, batch_size= test_batch_size, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset,shuffle=True, batch_size= batch_sizev, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset ,shuffle =True, batch_size= test_batch_size, num_workers=2)
 ROCloader = torch.utils.data.DataLoader(testset,batch_size=4)
 # %% Import Neural network
 
